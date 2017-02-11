50f2bac48735b23d99a8e3897834dc8675e4b983
Ning Sun <sunng@about.me>
2017-02-11 14:55:08 +0800

    (fix) adopt tokio 0.1 fix compilation error
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,16 @@ extern crate tokio_proto as tproto;
 extern crate tokio_service as tservice;
 extern crate futures;
 extern crate serde_json;
-extern crate bytes;
 extern crate byteorder;
 
 mod packets;
 mod codecs;
 
-use bytes::buf::BlockBuf;
-use tservice::{NewService, Service};
-use tproto::{server, Framed, Message};
-use tproto::pipeline::Server;
+use tservice::Service;
+use tproto::pipeline::ServerProto;
+use tcore::io::{Io, Framed};
 use tcore::reactor::Core;
-use futures::{Future, finished, Oneshot, failed, empty, BoxFuture, Async};
-use futures::stream::Empty;
+use futures::{future, Future, finished, Oneshot, failed, BoxFuture, Async};
 use serde_json::value::Value as Json;
 
 use std::collections::BTreeMap;
@@ -50,10 +47,9 @@ impl<T> Service for SlackerService<T>
     where T: Send + Sync + 'static
 {
     type Request = SlackerPacket<T>;
-    type Response = Message<SlackerPacket<T>, Empty<(), Self::Error>>;
+    type Response = SlackerPacket<T>;
     type Error = io::Error;
     type Future = BoxFuture<Self::Response, Self::Error>;
-    // type Fut = Finished<SlackerPacket<T>, ()>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
         match req {
@@ -70,9 +66,7 @@ impl<T> Service for SlackerService<T>
                                 serial_id: sreq.serial_id,
                                 result: result,
                             }))
-
                         })
-                        .map(Message::WithoutBody)
                         .map_err(|_| io::Error::new(io::ErrorKind::Other, "Oneshot canceled"))
                         .boxed()
                 } else {
@@ -81,50 +75,52 @@ impl<T> Service for SlackerService<T>
                         code: RESULT_CODE_NOT_FOUND,
                         serial_id: sreq.serial_id,
                     };
-                    finished(SlackerPacket::Error(error)).map(Message::WithoutBody).boxed()
+                    finished(SlackerPacket::Error(error)).boxed()
                 }
             }
             SlackerPacket::Ping(ref ping) => {
-                finished(SlackerPacket::Pong(SlackerPong { version: ping.version }))
-                    .map(Message::WithoutBody)
-                    .boxed()
+                finished(SlackerPacket::Pong(SlackerPong { version: ping.version })).boxed()
             }
-            _ => {
-                Box::new(failed(io::Error::new(io::ErrorKind::InvalidInput, "Unsupported packet")))
-            }
+            _ => failed(io::Error::new(io::ErrorKind::InvalidInput, "Unsupported packet")).boxed(),
         }
     }
+}
 
-    fn poll_ready(&self) -> Async<()> {
-        Async::Ready(())
+pub struct JsonSlacker;
+
+impl<T: Io + 'static> ServerProto<T> for JsonSlacker {
+    type Request = SlackerPacket<Json>;
+    type Response = SlackerPacket<Json>;
+    type Transport = Framed<T, JsonSlackerCodec>;
+    type BindTransport = io::Result<Framed<T, JsonSlackerCodec>>;
+
+    fn bind_transport(&self, io: T) -> io::Result<Framed<T, JsonSlackerCodec>> {
+        Ok(io.framed(JsonSlackerCodec))
     }
 }
 
 
-pub fn serve<T>(addr: SocketAddr, new_service: T) -> io::Result<()>
-    where T: NewService<Request = SlackerPacket<Json>,
-                        Response = Message<SlackerPacket<Json>, Empty<(), io::Error>>,
-                        Error = io::Error> + Send + 'static
-{
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
 
-    try!(server::listen(&handle, addr, move |socket| {
-        // Create the service
-        let service = try!(new_service.new_service());
+// pub fn serve<T>(addr: SocketAddr, new_service: T) -> io::Result<()>
+//     where T: NewService<Request = SlackerPacket<Json>,
+//                         Response = SlackerPacket<Json>,
+//                         Error = io::Error> + Send + 'static
+// {
+//     let mut core = Core::new().unwrap();
+//     let handle = core.handle();
 
-        // Create the transport
-        let codec = JsonSlackerCodec;
-        let transport = Framed::new(socket,
-                                    codec,
-                                    codec,
-                                    BlockBuf::default(),
-                                    BlockBuf::default());
+//     try!(server::listen(&handle, addr, move |socket| {
+//         // Create the service
+//         let service = try!(new_service.new_service());
 
-        // Return the pipeline server task
-        Server::new(service, transport)
-    }));
+//         // Create the transport
+//         let codec = JsonSlackerCodec;
+//         let transport = EasyFramed::new(socket, codec, codec);
 
-    core.run(empty::<(), ()>());
-    Ok(())
-}
+//         // Return the pipeline server task
+//         Server::new(service, transport)
+//     }));
+
+//     core.run(empty::<(), ()>());
+//     Ok(())
+// }
