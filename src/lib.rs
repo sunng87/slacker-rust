d4101f58e8362f9a78bc8568ea254f06b827c6dc
Ning Sun <sunng@about.me>
2017-02-12 11:26:19 +0800

    (refactor) move service stuff
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,11 @@ extern crate byteorder;
 
 mod packets;
 mod codecs;
+mod service;
 
-use tservice::{NewService, Service};
 use tproto::TcpServer;
 use tproto::pipeline::ServerProto;
 use tcore::io::{Io, Framed};
-use futures::Future;
-use futures::future::{ok, err, BoxFuture};
-use futures::sync::oneshot::Receiver;
 use serde_json::value::Value as Json;
 
 use std::collections::BTreeMap;
@@ -28,82 +25,9 @@ use std::net::SocketAddr;
 
 use packets::*;
 use codecs::*;
+use service::*;
 
-pub type RpcFn<T> = Box<Fn(&Vec<T>) -> Receiver<T> + Send + Sync + 'static>;
-
-pub struct SlackerService<T>
-    where T: Send + Sync + 'static
-{
-    functions: Arc<BTreeMap<String, RpcFn<T>>>,
-}
-
-impl<T> SlackerService<T>
-    where T: Send + Sync + 'static
-{
-    pub fn new(functions: Arc<BTreeMap<String, RpcFn<T>>>) -> SlackerService<T> {
-        SlackerService { functions: functions }
-    }
-}
-
-impl<T> Service for SlackerService<T>
-    where T: Send + Sync + 'static
-{
-    type Request = SlackerPacket<T>;
-    type Response = SlackerPacket<T>;
-    type Error = io::Error;
-    type Future = BoxFuture<Self::Response, Self::Error>;
-
-    fn call(&self, req: Self::Request) -> Self::Future {
-        match req {
-            SlackerPacket::Request(sreq) => {
-                debug!("getting request: {:?}", sreq.fname);
-                if let Some(f) = self.functions.get(&sreq.fname) {
-                    f(&sreq.arguments)
-                        .and_then(move |result| {
-                            debug!("getting results");
-                            ok(SlackerPacket::Response(SlackerResponse {
-                                version: sreq.version,
-                                code: RESULT_CODE_SUCCESS,
-                                content_type: sreq.content_type,
-                                serial_id: sreq.serial_id,
-                                result: result,
-                            }))
-                        })
-                        .map_err(|_| io::Error::new(io::ErrorKind::Other, "Oneshot canceled"))
-                        .boxed()
-                } else {
-                    let error = SlackerError {
-                        version: sreq.version,
-                        code: RESULT_CODE_NOT_FOUND,
-                        serial_id: sreq.serial_id,
-                    };
-                    ok(SlackerPacket::Error(error)).boxed()
-                }
-            }
-            SlackerPacket::Ping(ref ping) => {
-                ok(SlackerPacket::Pong(SlackerPong { version: ping.version })).boxed()
-            }
-            _ => err(io::Error::new(io::ErrorKind::InvalidInput, "Unsupported packet")).boxed(),
-        }
-    }
-}
-
-struct NewSlackerService<T>(Arc<BTreeMap<String, RpcFn<T>>>) where T: Send + Sync + 'static;
-
-impl<T> NewService for NewSlackerService<T>
-    where T: Send + Sync + 'static
-{
-    type Request = SlackerPacket<T>;
-    type Response = SlackerPacket<T>;
-    type Error = io::Error;
-    type Instance = SlackerService<T>;
-
-    fn new_service(&self) -> io::Result<Self::Instance> {
-        Ok(SlackerService::new(self.0.clone()))
-    }
-}
-
-pub struct JsonSlacker;
+struct JsonSlacker;
 
 impl<T: Io + 'static> ServerProto<T> for JsonSlacker {
     type Request = SlackerPacket<Json>;
---
+++ src/service.rs
@@ -0,0 +1,84 @@
+use std::io;
+use std::collections::BTreeMap;
+use std::sync::Arc;
+
+use futures::Future;
+use futures::future::{ok, err, BoxFuture};
+use futures::sync::oneshot::Receiver;
+use tservice::{NewService, Service};
+
+use packets::*;
+
+pub type RpcFn<T> = Box<Fn(&Vec<T>) -> Receiver<T> + Send + Sync + 'static>;
+
+pub struct SlackerService<T>
+    where T: Send + Sync + 'static
+{
+    functions: Arc<BTreeMap<String, RpcFn<T>>>,
+}
+
+impl<T> SlackerService<T>
+    where T: Send + Sync + 'static
+{
+    pub fn new(functions: Arc<BTreeMap<String, RpcFn<T>>>) -> SlackerService<T> {
+        SlackerService { functions: functions }
+    }
+}
+
+impl<T> Service for SlackerService<T>
+    where T: Send + Sync + 'static
+{
+    type Request = SlackerPacket<T>;
+    type Response = SlackerPacket<T>;
+    type Error = io::Error;
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    fn call(&self, req: Self::Request) -> Self::Future {
+        match req {
+            SlackerPacket::Request(sreq) => {
+                debug!("getting request: {:?}", sreq.fname);
+                if let Some(f) = self.functions.get(&sreq.fname) {
+                    f(&sreq.arguments)
+                        .and_then(move |result| {
+                            debug!("getting results");
+                            ok(SlackerPacket::Response(SlackerResponse {
+                                version: sreq.version,
+                                code: RESULT_CODE_SUCCESS,
+                                content_type: sreq.content_type,
+                                serial_id: sreq.serial_id,
+                                result: result,
+                            }))
+                        })
+                        .map_err(|_| io::Error::new(io::ErrorKind::Other, "Oneshot canceled"))
+                        .boxed()
+                } else {
+                    let error = SlackerError {
+                        version: sreq.version,
+                        code: RESULT_CODE_NOT_FOUND,
+                        serial_id: sreq.serial_id,
+                    };
+                    ok(SlackerPacket::Error(error)).boxed()
+                }
+            }
+            SlackerPacket::Ping(ref ping) => {
+                ok(SlackerPacket::Pong(SlackerPong { version: ping.version })).boxed()
+            }
+            _ => err(io::Error::new(io::ErrorKind::InvalidInput, "Unsupported packet")).boxed(),
+        }
+    }
+}
+
+pub struct NewSlackerService<T>(pub Arc<BTreeMap<String, RpcFn<T>>>) where T: Send + Sync + 'static;
+
+impl<T> NewService for NewSlackerService<T>
+    where T: Send + Sync + 'static
+{
+    type Request = SlackerPacket<T>;
+    type Response = SlackerPacket<T>;
+    type Error = io::Error;
+    type Instance = SlackerService<T>;
+
+    fn new_service(&self) -> io::Result<Self::Instance> {
+        Ok(SlackerService::new(self.0.clone()))
+    }
+}
