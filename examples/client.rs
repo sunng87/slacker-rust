a17ca064b33438a14c2ba0093691d4c5e14b5073
Ning Sun <sunng@about.me>
2017-03-15 22:05:32 +0800

    (feat) added client example
    Signed-off-by: Ning Sun <sunng@about.me>

---
+++ examples/client.rs
@@ -0,0 +1,25 @@
+extern crate slacker;
+extern crate futures;
+extern crate tokio_core;
+extern crate serde_json;
+extern crate env_logger;
+
+use futures::Future;
+use tokio_core::reactor::Core;
+use slacker::Client;
+
+fn main() {
+    drop(env_logger::init());
+
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let addr = "127.0.0.1:3299".parse().unwrap();
+    core.run(Client::connect(&addr, &handle)
+                 .and_then(|c| c.rpc_call("rust.test", "echo", Vec::new()))
+                 .and_then(|r| {
+                     println!("{:?}", r);
+                     Ok(())
+                 }))
+        .unwrap();
+}
